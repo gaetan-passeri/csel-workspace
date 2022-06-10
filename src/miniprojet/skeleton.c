90213bd63c3fcfeee04bee0f04b5aaab541b7a22
Gaetan Passeri <gaetan.passeri@hesge.ch>
2022-06-10 18:52:41 +0200

    kernel module -> timer & temp

--- src/miniprojet/skeleton.c
+++ src/miniprojet/skeleton.c
@@ -7,39 +7,50 @@
 #include <linux/timer.h>	// need for timer
 #include <linux/moduleparam.h>	// needed for module parameters
 
-static char* text = "dummy text";
-module_param(text, charp, 0664);
-static int  elements = 1;
-module_param(elements, int, 0);
+enum Mode {AUTO, MAN};
+static int current_mode = AUTO;
+module_param(current_mode, int, 0);
+static int  frequency_Hz = 2;		// 2, 5, 10, 20
+module_param(frequency_Hz, int, 0);
 
 const char *cpu_thermal = "cpu-thermal";
 
-static int timer_callback(){
+struct timer_list mytimer;
+
+void timer_callback(void){
 	struct thermal_zone_device* thermal_zone = thermal_zone_get_zone_by_name (cpu_thermal);
-	int temp;
-	
+	int temp;	
+
 	thermal_zone_get_temp(thermal_zone, &temp);
 
 	pr_info("CPU temperature : %d",temp);
+
+	//add_timer(&mytimer); // restart timer
 }
 
 static int __init skeleton_init(void)
 {
-	pr_info ("Linux module fan management loaded\n");	
+	pr_info ("Linux module fan management loaded\n");
 
-	timer_setup(timer, timer_callback, TIMER_MONOTONIC);
+	// set timer frequency
+	timer_setup(&mytimer, (void *) timer_callback, CLOCK_MONOTONIC);
+	mod_timer(&mytimer, 1000000000);
+	add_timer(&mytimer);
+	
+	printk("Timer Started\r\n");
+	//timer_setup(timer, timer_callback, TIMER_MONOTONIC);
 
 	return 0;
 }
 
 static void __exit skeleton_exit(void)
 {
-	pr_info ("Linux module skeleton unloaded\n");
+	pr_info ("Linux module fan management unloaded\n");
 }
 
 module_init (skeleton_init);
 module_exit (skeleton_exit);
 
 MODULE_AUTHOR ("Glenn Muller <glenn.mullerar@hes-so.ch>");
-MODULE_DESCRIPTION ("Module skeleton");
+MODULE_DESCRIPTION ("Linux module fan management");
 MODULE_LICENSE ("GPL");
