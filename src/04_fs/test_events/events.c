f4c3f5dae7aa71c30a96bda4a4dd5b49133af3f8
glennmullerar <glenn.mullerar@hes-so.ch>
2022-06-17 14:00:37 +0000

    epoll on switchs done

--- src/04_fs/test_events/events.c
+++ src/04_fs/test_events/events.c
@@ -9,6 +9,7 @@
 #include <sys/epoll.h>
 #include <sys/select.h>
 #include <stdio.h>
+#include <sys/timerfd.h>
 
 
 /*
@@ -18,64 +19,67 @@
 #define GPIO_EXPORT   "/sys/class/gpio/export"
 #define GPIO_UNEXPORT "/sys/class/gpio/unexport"
 #define GPIO_LED      "/sys/class/gpio/gpio10"
+#define GPIO_K1      "/sys/class/gpio/gpio0"
+#define GPIO_K2      "/sys/class/gpio/gpio2"
+#define GPIO_K3      "/sys/class/gpio/gpio3"
 #define LED           "10"
 #define SW_K1         "0"
 #define SW_K2         "2"
 #define SW_K3         "3"
-int yes = 1;
 
+const char* GPIO_SWITCHS[] = {"/sys/class/gpio/gpio0", "/sys/class/gpio/gpio2", "/sys/class/gpio/gpio3"};
 
-static int open_switchs(){
-    int f = open(GPIO_UNEXPORT, O_WRONLY);
-    write(f, SW_K1, strlen(SW_K1));
-    close(f);
+const char* SW[] = {"0", "2", "3"};
 
-    f = open(GPIO_EXPORT,O_WRONLY);
-    write(f, SW_K1, strlen(SW_K1));
-    close(f);
+static void open_switchs(){
+    int f;
+    
+    for(int i=0;i<3;i++){
+        char direction[31];
+        char edge[26];
+        
+        // creat string to config GPIO
+        strcpy(direction,GPIO_SWITCHS[i]);
+        strcat(direction,"/direction");
 
-    // config pin
-    f = open(SW_K1 "/direction", O_WRONLY);
-    write(f, "in", 2);
-    close(f);
+        strcpy(edge,GPIO_SWITCHS[i]);
+        strcat(edge,"/edge");
 
+        // reset previouse config
+        f = open(GPIO_UNEXPORT, O_WRONLY);
+        write(f, SW[i], strlen(SW[i]));
+        close(f);
 
-    f = open(GPIO_UNEXPORT, O_WRONLY);
-    write(f, SW_K2, strlen(SW_K2));
-    close(f);
+        // export GPIO
+        f = open(GPIO_EXPORT,O_WRONLY);
+        write(f, SW[i], strlen((SW[i])));
+        close(f);
 
-    f = open(GPIO_EXPORT,O_WRONLY);
-    write(f, SW_K2, strlen(SW_K2));
-    close(f);
+        // config pin as INPUT
+        f = open(direction, O_WRONLY);
+        write(f, "in", 2);
+        close(f);
 
-    // config pin
-    f = open(SW_K2 "/direction", O_WRONLY);
-    write(f, "in", 2);
-    close(f);
+        // config pin for rising event 
+        f = open(edge, O_WRONLY);
+        write(f, "rising", 6);
+        close(f);
 
-    return f;
+    }
 }
 
 
 int main(int argc, char* argv[])
 {
-    printf("Hello main\n");
     // ----------- multiplex  ----------- 
     open_switchs();
 
-    int f_k1 = open("/sys/class/gpio/gpio0/value",O_RDONLY);
-    int f_k2 = open("/sys/class/gpio/gpio2/value",O_RDONLY);
+    int fd_switchs[3];
+    fd_switchs[0] = open("/sys/class/gpio/gpio0/value",O_RDONLY);
+    fd_switchs[1] = open("/sys/class/gpio/gpio2/value",O_RDONLY);
+    fd_switchs[2] = open("/sys/class/gpio/gpio3/value",O_RDONLY);
     
-    if (f_k1 == -1) {
-        printf("file k1 not found!\n");
-        exit(1);
-    } 
-    if(f_k2 == -1){
-        printf("file k2 not found\n");
-        exit(1);
-    }
-
-    printf("fd k1 : %d | fd k2 : %d\n",f_k1,f_k2);
+#if 1
 
     int epfd = epoll_create1(0);
 
@@ -83,38 +87,87 @@ int main(int argc, char* argv[])
         printf("ERROR : epoll create %d\n",epfd);
         exit(1);
     }
+    printf("epoll created\n");
 
-    struct epoll_event event = {
-        .events = EPOLLIN | EPOLLET,// EPOLLET,
-        .data.fd = f_k1,
-    };
+    // struct epoll_event event_k2 = {
+    //     .events = EPOLLET,
+    //     .data.fd = f_k2,
+    // };
 
-    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, f_k1, &event);
+    // struct epoll_event event_k3 = {
+    //     .events = EPOLLET,
+    //     .data.fd = f_k3,
+    // };
+
+    for(int i=0;i<3;i++){
+    struct epoll_event event_switch[3];
+
+    if (fd_switchs[i] == -1) {
+        printf("file k%d not found!\n",(i+1));
+        exit(1);
+    } 
+    printf("fd k%d : %d |",(i+1),fd_switchs[i]);
+
+    event_switch[i].events = EPOLLET;
+    event_switch[i].data.fd = fd_switchs[i];
+
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd_switchs[i], &event_switch[i]);
     if (ret == -1){
         printf("ERROR : epoll ctl %d\n", ret);
         exit(1);
     }    
+}
 
+#endif
+
+#if 0
+    printf("events done \n");
+    
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, f_k1, &event_k1);
+    if (ret == -1){
+        printf("ERROR : epoll ctl %d\n", ret);
+        exit(1);
+    }    
+    
+    printf("event add to epoll k1\n");
+    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, f_k2, &event_k2);
+    if (ret == -1){
+        printf("ERROR : epoll ctl %d\n", ret);
+        exit(1);
+    }   
+    
+    printf("event add to epoll k2\n");
+    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, f_k3, &event_k3);
+    if (ret == -1){
+        printf("ERROR : epoll ctl %d\n", ret);
+        exit(1);
+    }   
+
+    printf("event add to epoll k3\n");
+#endif
+
+#if 1
+    struct epoll_event events[5];
     while (1)
     {
-        int nr = epoll_wait(epfd, &event, 2, -1);
+        int nr = epoll_wait(epfd, events, 5, -1);
         if (nr == -1)
             printf("ERROR : epoll wait\n");
-        printf("wait\n");
 
-
+        printf("number of event : %d\n",nr);
         for (int i=0; i<nr; i++) {
-            // printf ("event=%ld on fd=%d\n", events[i].events, events[i].data.fd);
-            printf("event number : %d\n",nr);
-
-            printf ("event=%ld on fd=%d\n", event.events, event.data.fd);
-            char buffer;
-            read(event.data.fd, &buffer,1);
-            printf("read : %c\n",buffer);
-            // operation on events[i].data.fd can be performed without blocking... 
+            if(events[i].data.fd == fd_switchs[0]){
+                printf("increase fan speed\n");
+            }
+            else if(events[i].data.fd == fd_switchs[1]){
+                printf("decrease fans speed\n");
+            }
+            else if(events[i].data.fd == fd_switchs[2]){
+                printf("change mode \n");
+            }
         }
-        printf("end for loop\n");
     }
+#endif   
     
     return 0;
 }
